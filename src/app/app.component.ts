8685beb59e8ae3fb0ede0f5f41edfb6c6b7e8161
Federico Kurganoff <kfederico_matias@hotmail.com>
2024-03-02 09:50:48 -0300

    primeros intentos de hacer conexion con API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'MzTnrSPA';
+export class AppComponent implements OnInit {
+  title = 'MzTnrStats SPA';
+  torneos: any;
+  
+  constructor(private http: HttpClient) {}
+  
+  ngOnInit(): void {
+    this.http.get('https://localhost:5117/api/Torneos').subscribe({
+      next: response => this.torneos = response,
+      error: error => console.log(error),
+      complete: () => console.log('Request a GET equipos completado'),
+    })
+  }
+
+  
 }
