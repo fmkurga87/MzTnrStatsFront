bf4a444b189057e0e9cee6091d675213d3117251
Federico Kurganoff <kfederico_matias@hotmail.com>
2024-05-03 07:10:08 -0300

    Comenzando a mostrar historial

--- src/app/_components/partidos/historico/historico-card/historico-card.component.ts
+++ src/app/_components/partidos/historico/historico-card/historico-card.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PartidosService } from '../../../../_services/partidos.service';
+import { BuscarHistorialResponse } from '../../../../_models/partidos/buscar-historial-response';
 
 @Component({
   selector: 'app-historico-card',
   templateUrl: './historico-card.component.html',
   styleUrl: './historico-card.component.css'
 })
-export class HistoricoCardComponent {
+export class HistoricoCardComponent implements OnInit{
+  historico : BuscarHistorialResponse | undefined;
 
+  constructor(private partidosService : PartidosService) 
+  {  
+    
+  }
+  ngOnInit(): void {
+    this.loadHistorico();
+  }
+
+  loadHistorico()
+  {
+    //const equipo =  this.route.snapshot.paramMap.get('idEquipo');
+    //if (!equipo) return;
+    this.partidosService.getHistorico().subscribe({
+                                                  next: response =>{ 
+                                                                      this.historico = response
+                                                                    },
+                                                  error: error => console.log(error)
+                                                })
+  }
 }
--- src/app/_models/partidos/buscar-historial-response.ts
+++ src/app/_models/partidos/buscar-historial-response.ts
@@ -3,8 +3,10 @@ import { ResumenPartido } from "./resumenPartido";
 
 export interface BuscarHistorialResponse extends baseResponse{
     totalPartidos: number;
+    equipo1: string;
     ganadosEquipo1: number;
     empatados: number;
+    equipo2: string;
     ganadosEquipo2: number;
     partidos: ResumenPartido[];
 }
